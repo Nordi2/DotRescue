581fe101968ed8e03d79260f9299c5ec3ee7dc00
Nordik <igor.golyshev.01@bk.ru>
2025-04-01 18:37:25 +0300

    feat : AddNaughtyAttributes

---
+++ Assets/_Project/Scripts/Infrastructure/GameRunner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Infrastructure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        [SerializeField] private GameBootstrapper _gameBootstrapper;
+
+        private void Awake()
+        {
+            GameBootstrapper gameBootstrap = FindObjectOfType<GameBootstrapper>();
+
+            if (gameBootstrap is null)
+                Instantiate(_gameBootstrapper);
+        }
+    }
+}
